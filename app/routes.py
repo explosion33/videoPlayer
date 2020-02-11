eaf4ff3c132ee35db420e087634dd3b7ca8e4343
explosion33 <38818379+explosion33@users.noreply.github.com>
2020-02-10 21:07:14 -0800

    disabled going forward if next cideo does not exist

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,7 @@
 from flask import render_template, flash, redirect, request, url_for
 from werkzeug import secure_filename
 from app import app
+import os
 
 play = 1
 
@@ -14,7 +15,28 @@ def index():
 @app.route("/increment/<amount>")
 def increment(amount):
     global play
+
+    vids = os.listdir(os.getcwd() + "/app/static/videos")
+
+
     play += int(amount)
     if play == 0:
         play = 1
+
+    nums = []
+    for vid in vids:
+        vid = vid[::-1]
+        vid = vid[4::]
+        num = ""
+        for i in vid:
+            if i != "_":
+                num += i
+            else:
+                break
+        num = num[::-1]
+        nums.append(int(num))
+
+    if play not in nums:
+        play -= int(amount)
+    
     return redirect(url_for("index"))
